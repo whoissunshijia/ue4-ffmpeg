49e8930d49eb5f276ca1c76caf8f1a450f35325f
Sunshijia_Family <iamsunshijia@gmail.com>
2020-12-04 21:23:15 +0800

    先暂时把 编码 sws crash 修改  后续重构

--- Plugins/UFFmpeg/Source/UFFmpeg/Private/EncoderThread.cpp
+++ Plugins/UFFmpeg/Source/UFFmpeg/Private/EncoderThread.cpp
@@ -129,11 +129,11 @@ void FEncoderThread::RunEncode()
 
 	{
 		FScopeLock ScopeLock1(&VideoBufferMutex);
-		IsNeedEncode=videobuffer_queue->PrcessEncodeData();
-	}
+		IsNeedEncode = videobuffer_queue->PrcessEncodeData()
 
-	if(IsNeedEncode)
-		EncodeVideo();
+		if (IsNeedEncode)
+			EncodeVideo();
+	}
 }
 
 void FEncoderThread::EncodeVideo()
